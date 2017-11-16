4c36229fd55e321a2c79dd0bad94ded0c89a60b8
wsabin19 <wsabin19@hanoverstudents.org>
2017-11-16 10:21:03 -0500

    Added -Money System -Betting System -Tracking Bets -Validation of Bets -Stopping Game if not enough Money

--- chapter 04/face.py
+++ chapter 04/face.py
@@ -1,10 +1,36 @@
 from graphics import *
+
+
 def main():
-    win = GraphWin("Face Drawing",400, 400)
+    win = GraphWin("picture", 800, 800)
+    win.setCoords(0, 0, 20, 20)
+    ground = Rectangle(Point(0, 0),Point(20, 3))
+    ground.setFill(color_rgb(102, 51, 0))
+    ground.draw(win)
 
-    #here is where the face is drawn
+    grass = Rectangle(Point(0, 3), Point(20, 4))
+    grass.setFill(color_rgb(102, 204, 0))
+    grass.draw(win)
 
-    win.getMouse()
+    trunk = Rectangle(Point(8, 4), Point(10, 10))
+    trunk.setFill(color_rgb(153, 76, 0))
+    trunk.draw(win)
+
+    leaves = Circle(Point(9, 13), 4)
+    leaves.setFill('green')
+    leaves.draw(win)
+
+
+    cloudA = Oval(Point(1, 17), Point(5, 19))
+    cloudA.setFill('white')
+    cloudA.draw(win)
+
+    cloudB = Oval(Point(13, 15), Point(17, 18))
+    cloudB.setFill('white')
+    cloudB.draw(win)
+
+
+    input("Press any key to quit")
 
 
 
